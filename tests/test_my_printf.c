7c26d4f6bf528cecaa57bdb4967540ac78d1eae5
Stanislas Gaumain <stanislas.gaumain@epitech.eu>
2025-10-14 19:37:40 +0200

    :zap: comparing directly the outpur of printf in tu

--- tests/test_my_printf.c
+++ tests/test_my_printf.c
@@ -6,57 +6,46 @@
 */
 
 #include <criterion/criterion.h>
-#include <criterion/redirect.h>
 #include "../include/my.h"
+#include <stdio.h>
 
-void redirect_all_std(void)
+Test(my_printf, simple_string)
 {
-    cr_redirect_stdout();
-    cr_redirect_stderr();
+    cr_assert_eq(my_printf("hello world"), printf("hello world"));
 }
 
-Test(mini_printf, simple_string, .init = redirect_all_std)
-{
-    mini_printf("hello world");
-    cr_assert_stdout_eq_str("hello world");
-}
-
-Test(mini_printf, one_argument, .init = redirect_all_std)
+Test(my_printf, one_argument)
 {
     char str[6] = "world";
 
-    mini_printf("Hello %s\n", str);
-    cr_assert_stdout_eq_str("Hello world\n");
+    cr_assert_eq(my_printf("Hello %s", str), printf("Hello %s", str));
 }
-
-Test(mini_printf, more_argument, .init = redirect_all_std)
+/*
+Test(my_printf, more_argument)
 {
     int nb = 21;
 
-    mini_printf("If you multiple %d by %d, the result is %i.\n", nb, 2, nb * 2);
-    cr_assert_stdout_eq_str("If you multiple 21 by 2, the result is 42.\n");
+    cr_assert_eq(my_printf("If you multiple %d by %d, the result is %i.", nb, 2, nb * 2), printf("If you multiple %d by %d, the result is %i.", nb, 2, nb * 2));
 }
 
-Test(mini_printf, lot_of_arguments_and_percentages, .init = redirect_all_std)
+Test(my_printf, lot_of_arguments_and_percentages)
 {
     char str[8] = "Epitech";
 
-    mini_printf("The word %%%s%% has %i characters.\n", str, my_strlen(str));
-    cr_assert_stdout_eq_str("The word %Epitech% has 7 characters.\n");
+    cr_assert_eq(my_printf("The word %%%s%% has %i characters.\n", str, my_strlen(str)), printf("The word %%%s%% has %i characters.\n", str, my_strlen(str)));
 }
 
-Test(mini_printf, invalid_flag, .init = redirect_all_std)
+Test(my_printf, invalid_flag)
 {
     char str[8] = "Epitech";
 
-    mini_printf("The word %%%s%% has %k characters.\n", str);
-    cr_assert_stdout_eq_str("The word %Epitech% has %k characters.\n");
+    cr_assert_eq(my_printf("The word %%%s%% has %k characters.\n", str), printf("The word %%%s%% has %k characters.\n", str));
 }
 
-Test(mini_printf, percentage_alone, .init = redirect_all_std)
+Test(mini_printf, percentage_alone)
 {
     char str[8] = "Epitech";
 
-    mini_printf("The word %%%s%% has % characters.\n", str);
-    cr_assert_stdout_eq_str("The word %Epitech% has  characters.\n");
+    cr_assert_eq(my_printf("The word %%%s%% has % characters.\n", str), printf("The word %%%s%% has % characters.\n", str));
 }
+*/
