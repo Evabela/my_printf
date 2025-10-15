d4b1a2abc7c8b5be93288528bdacb4ed05f51f0b
Eva Belanger <eva.belanger@epitech.eu>
2025-10-15 22:47:34 +0200

    :recycle: Add flag_X function, but the name have to be changed

--- flags_hexa.c
+++ flags_hexa.c
@@ -7,12 +7,10 @@
 #include <stdarg.h>
 #include "include/my.h"
 
-int int_to_hexa(int nb)
+int int_to_hexa(int nb, char *ref)
 {
-    char ref[16] = {'1', '2', '3', '4', '5', '6', '7', '8', '9',
-                    'a', 'b', 'c', 'd', 'e', 'f'};
     if (nb != 0){
-        int_to_hexa(nb / 16);
+        int_to_hexa(nb / 16, ref);
         my_putchar(ref[(nb % 16) - 1]);
     }
     return 0;
@@ -20,9 +18,25 @@ int int_to_hexa(int nb)
 
 int flag_x(va_list list, int nb_char)
 {
+    char ref[16] = {'1', '2', '3', '4', '5', '6', '7', '8', '9',
+        'a', 'b', 'c', 'd', 'e', 'f'};
     int nb = va_arg(list, int);
 
-    int_to_hexa(nb);
+    int_to_hexa(nb, ref);
+    while (nb != 0){
+        nb = nb / 16;
+        nb_char++;
+    }
+    return nb_char;
+}
+
+int flag_X(va_list list, int nb_char)
+{
+    char ref[16] = {'1', '2', '3', '4', '5', '6', '7', '8', '9',
+        'A', 'B', 'C', 'D', 'E', 'F'};
+    int nb = va_arg(list, int);
+
+    int_to_hexa(nb, ref);
     while (nb != 0){
         nb = nb / 16;
         nb_char++;
