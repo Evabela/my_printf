d24451a3aac9e870307c495cc7569b366f336dd6
Eva Belanger <eva.belanger@epitech.eu>
2025-10-15 19:32:47 +0200

    :sparkles: flag_x added

--- flags_hexa.c
+++ flags_hexa.c
@@ -10,15 +10,22 @@
 int int_to_hexa(int nb)
 {
     char ref[16] = {'1', '2', '3', '4', '5', '6', '7', '8', '9',
-                    'A', 'B', 'C', 'D', 'E', 'F'};
+                    'a', 'b', 'c', 'd', 'e', 'f'};
     if (nb != 0){
         int_to_hexa(nb / 16);
         my_putchar(ref[(nb % 16) - 1]);
-    } else
-        return 0;
+    }
+    return 0;
 }
 
 int flag_x(va_list list, int nb_char)
 {
-    return 0;
+    int nb = va_arg(list, int);
+
+    int_to_hexa(nb);
+    while (nb != 0){
+        nb = nb / 16;
+        nb_char++;
+    }
+    return nb_char;
 }
