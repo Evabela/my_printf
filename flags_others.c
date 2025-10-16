9d5ae9a5d7a0283572dd09172a75aa342f70a455
Eva Belanger <eva.belanger@epitech.eu>
2025-10-16 14:00:43 +0200

    :sparkles: Flag %p finished

--- flags_others.c
+++ flags_others.c
@@ -8,7 +8,27 @@
 #include <stdarg.h>
 #include "include/my.h"
 
+int long_to_hexa(unsigned long nb, char *ref)
+{
+    if (nb != 0){
+        long_to_hexa(nb / 16, ref);
+        my_putchar(ref[(nb % 16) - 1]);
+    }
+    return 0;
+}
+
 int flag_p(va_list list, int nb_char)
 {
+    unsigned long adress = (unsigned long) va_arg(list, void *);
+    char ref[16] = {'1', '2', '3', '4', '5', '6', '7', '8', '9',
+        'a', 'b', 'c', 'd', 'e', 'f'};
+
+    my_putstr("0x");
+    nb_char += 2;
+    long_to_hexa(adress, ref);
+    while (adress != 0){
+        adress = adress / 16;
+        nb_char++;
+    }
     return nb_char;
 }
