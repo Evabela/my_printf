d414141dbaea7d7dfc59da7ef806515be425b7c0
Eva Belanger <eva.belanger@epitech.eu>
2025-10-16 21:53:27 +0200

    :poop: Flag %f and %F doesn't work in flag_ints.c

--- flags_ints.c
+++ flags_ints.c
@@ -8,6 +8,15 @@
 #include "include/my.h"
 #include <stdarg.h>
 
+int my_put_dbl(double nb)
+{
+    if (nb > 0){
+        my_put_dbl(nb / 10);
+        my_putchar((int) nb % 10 + '0');
+    }
+    return 0;
+}
+
 int flag_u(va_list list, int nb_char)
 {
     unsigned int nb = va_arg(list, unsigned int);
@@ -25,13 +34,21 @@ int flag_i(va_list list, int nb_char)
 int flag_f(va_list list, int nb_char)
 {
     double nb = va_arg(list, double);
+    int ent = (int) nb;
 
     my_put_nbr((int) nb);
     nb_char += my_intlen((int) nb);
     my_putchar('.');
-    nb1 -= (int) nb;
-    my_put_nbr((int) (nb * 1000000));
+    nb -= ent * 1000000;
+    my_put_dbl((int) (nb * 1000000));
     nb_char += my_intlen((int) (nb * 1000000));
+
+    my_put_dbl(nb2);
+    my_putchar('.');
+    nb2 *= 1000000;
+    nb2 -= ent * 1000000;
+    my_put_dbl(nb2);
+
     return nb_char;
 }
 
