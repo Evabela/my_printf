153c5da2028ca8fd86fac9751ef1d1461e62902c
Eva Belanger <eva.belanger@epitech.eu>
2025-10-17 17:41:40 +0200

    :sparkles: finished flag %f and implementing a function for diplaying double

--- flags_ints.c
+++ flags_ints.c
@@ -8,13 +8,28 @@
 #include "include/my.h"
 #include <stdarg.h>
 
-int my_put_dbl(double nb)
+int my_put_dbl(double temp, double nb, double ten, int nb_char)
 {
-    if (nb > 0){
-        my_put_dbl(nb / 10);
-        my_putchar((int) nb % 10 + '0');
+    while (nb >= 1.0){
+	while (temp >= 10.0){
+            temp = temp / 10.0;
+            ten *= 10;
+        }
+        my_putchar((int) temp + '0');
+        nb = nb - (int) temp * ten / 10;
+	nb_char++;
+        temp = nb;
+        ten = 10;
     }
-    return 0;
+    my_putchar('.');
+    nb_char++;
+    for (int i = 0; i < 6; i++){
+        nb *= 10;
+        my_putchar((int) nb + '0');
+        nb_char++;
+        nb -= (int) nb;
+    }
+    return nb_char;
 }
 
 int flag_u(va_list list, int nb_char)
@@ -34,21 +49,15 @@ int flag_i(va_list list, int nb_char)
 int flag_f(va_list list, int nb_char)
 {
     double nb = va_arg(list, double);
-    int ent = (int) nb;
-
-    my_put_nbr((int) nb);
-    nb_char += my_intlen((int) nb);
-    my_putchar('.');
-    nb -= ent * 1000000;
-    my_put_dbl((int) (nb * 1000000));
-    nb_char += my_intlen((int) (nb * 1000000));
-
-    my_put_dbl(nb2);
-    my_putchar('.');
-    nb2 *= 1000000;
-    nb2 -= ent * 1000000;
-    my_put_dbl(nb2);
-
+   
+    if (nb < 0.0){
+        my_putchar('-');
+        nb_char++;
+        nb -= 0.0000005;
+        nb = - nb;
+    } else
+        nb += 0.0000005;
+    nb_char = my_put_dbl(nb, nb, 10.0, nb_char);
     return nb_char;
 }
 
@@ -56,11 +65,5 @@ int flag_fu(va_list list, int nb_char)
 {
     double nb = va_arg(list, double);
 
-    my_put_nbr((int) nb);
-    nb_char += my_intlen((int) nb);
-    my_putchar('.');
-    nb1 -= (int) nb;
-    my_put_nbr((int) (nb * 1000000));
-    nb_char += my_intlen((int) (nb * 1000000));
     return nb_char;
 }
