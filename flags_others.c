10973db8c20304fc0a4bc784c6de5c1b5c8b2045
Eva Belanger <eva.belanger@epitech.eu>
2025-10-17 11:23:29 +0200

    :recycle: missing 0 in flags for hexa

--- flags_others.c
+++ flags_others.c
@@ -12,7 +12,7 @@ int long_to_hexa(unsigned long nb, char *ref)
 {
     if (nb != 0){
         long_to_hexa(nb / 16, ref);
-        my_putchar(ref[(nb % 16) - 1]);
+        my_putchar(ref[(nb % 16)]);
     }
     return 0;
 }
@@ -20,7 +20,7 @@ int long_to_hexa(unsigned long nb, char *ref)
 int flag_p(va_list list, int nb_char)
 {
     unsigned long adress = (unsigned long) va_arg(list, void *);
-    char ref[16] = {'1', '2', '3', '4', '5', '6', '7', '8', '9',
+    char ref[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
         'a', 'b', 'c', 'd', 'e', 'f'};
 
     my_putstr("0x");
