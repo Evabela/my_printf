8c8335410584a655bc98e577db341f5da7a0d0a9
Eva Belanger <eva.belanger@epitech.eu>
2025-10-23 22:46:12 +0200

    :sparkles: finished checking for any flags and letters conflicts in correct_flags_2

--- real_flags.c
+++ real_flags.c
@@ -20,6 +20,19 @@ void correct_flags(int *tab)
 
 void correct_flags2(int *tab)
 {
-    //char flags[5] = {'+', '-', ' ', '#', '0', letter, w, p};
-    if (tab[0] == 1 && (tab[5] == 'u' || tab[5] == 'o' || tab[5] == ''))
+    if (tab[0] == 1 &&
+        (tab[5] == 'u' || tab[5] == 'o' || tab[5] == 'x' || tab[5] == 'X' ||
+            tab[5] == 'c' || tab[5] == 's' || tab[5] == 'p' || tab[5] == '%'))
+        tab[0] = 0;
+    if (tab[2] == 1 &&
+        (tab[5] == 'u' || tab[5] == 'o' || tab[5] == 'x' || tab[5] == 'X' ||
+            tab[5] == 'c' || tab[5] == 's' || tab[5] == 'p' || tab[5] == '%'))
+        tab[2] = 0;
+    if (tab[3] == 1 &&
+        (tab[5] == 'u' || tab[5] == 'd' || tab[5] == 'i' || tab[5] == 'c' ||
+            tab[5] == 's' || tab[5] == 'p' || tab[5] == '%'))
+        tab[3] = 0;
+    if (tab[4] == 1 &&
+        (tab[5] == 'c' || tab[5] == 's' || tab[5] == 'p' || tab[5] == '%'))
+        tab[4] = 0;
 }
