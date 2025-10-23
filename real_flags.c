845164fa91ab79260ebfd34668a4cd68a29203b4
Eva Belanger <eva.belanger@epitech.eu>
2025-10-23 21:25:10 +0200

    :sparkles: starting correcting tab with all letters and their flags

--- real_flags.c
+++ real_flags.c
@@ -6,14 +6,20 @@
 */
 #include "include/my.h"
 
-void correct_flags(int *tab, int width, int precision)
+void correct_flags(int *tab)
 {
-    if (tab[1] == 1 && width == 0)
+    if (tab[1] == 1 && tab[6] == 0)
         tab[1] = 0;
-    if (tab[2] == 1 && flag[0] == 1)
+    if (tab[2] == 1 && tab[0] == 1)
         tab[2] = 0;
     if (tab[4] == 1 && tab[1] == 1)
         tab[4] = 0;
-    if (tab[4] == 1 && width == 0)
+    if (tab[4] == 1 && tab[6] == 0)
         tab[4] = 0;
+}
+
+void correct_flags2(int *tab)
+{
+    //char flags[5] = {'+', '-', ' ', '#', '0', letter, w, p};
+    if (tab[0] == 1 && (tab[5] == 'u' || tab[5] == 'o' || tab[5] == ''))
 }
