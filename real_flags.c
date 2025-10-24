7f98fb8b97c5055db4cd9f95913aa5b5624372cf
Stanislas Gaumain <stanislas.gaumain@epitech.eu>
2025-10-24 11:09:24 +0200

    :sparkles: adding the space function

--- real_flags.c
+++ real_flags.c
@@ -8,6 +8,8 @@
 
 void redirect_flag_nb(int *tab, int nb, int *nb_char)
 {
+    int char_nb = 0;
+
     correct_flags(tab);
     correct_flags2(tab);
     if (tab[0] == 1 && nb >= 0)
@@ -18,10 +20,14 @@ void redirect_flag_nb(int *tab, int nb, int *nb_char)
         function_pos(nb_char, ' ');
     if (tab[3] == 1)
         function_sharp(nb_char, tab[5]);
-    if (tab[4] == 1)
-        function_spaces('0', tab[6], nb_char, ...); //idem ici
-    if (tab[6] > 0)
-        function_space(' ', tab[6], nb_char, ...);
+    if (tab[4] == 1) {
+        char_nb = tab[6] - my_int_len(nb); 
+        function_spaces('0', nb_char, char_nb);
+    }
+    if (tab[6] > 0) {
+        char_nb = tab[6] - my_int_len(nb);
+        function_space(' ', nb_char, char_nb);
+    }
 }
 
 void redirect_flag_dblnb(int *tab, double nb, int *nb_char)
