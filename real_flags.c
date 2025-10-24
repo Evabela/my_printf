92a68dc156a5a8198c7a2c60bf7a236660ded0c0
Eva Belanger <eva.belanger@epitech.eu>
2025-10-24 09:50:06 +0200

    :recycle: copy and paste redirect_flags for differents types (ints only for the moment)

--- real_flags.c
+++ real_flags.c
@@ -6,44 +6,48 @@
 */
 #include "include/my.h"
 
-void correct_flags(int *tab)
+void redirect_flag_nb(int *tab, int nb, int *nb_char)
 {
-    if (tab[1] == 1 && tab[6] == 0)
-        tab[1] = 0;
-    if (tab[2] == 1 && tab[0] == 1)
-        tab[2] = 0;
-    if (tab[4] == 1 && tab[1] == 1)
-        tab[4] = 0;
-    if (tab[4] == 1 && tab[6] == 0)
-        tab[4] = 0;
+    correct_flags(tab);
+    correct_flags2(tab);
+    if (tab[0] == 1 && nb >= 0)
+        function_pos(nb_char, '+');
+    if (tab[1] == 1)
+        function_alignement(nb, nb_char, tab[6]); // peut-être plus appeler une fonction qui récupère un nombre de charactère en fonction de ce qui est affiché et le passe en param
+    if (tab[2] == 1  && nb >= 0)
+        function_pos(nb_char, ' ');
+    if (tab[3] == 1)
+        function_sharp(nb_char, tab[5]);
+    if (tab[4] == 1)
+        function_spaces('0', tab[6], nb_char, ...);
+    if (tab[6] > 0)
+        function_space(' ', tab[6], nb_char, ...);
 }
 
-void correct_flags2(int *tab)
+void redirect_flag_dblnb(int *tab, double nb, int *nb_char)
 {
-    if (tab[0] == 1 &&
-        (tab[5] == 'u' || tab[5] == 'o' || tab[5] == 'x' || tab[5] == 'X' ||
-            tab[5] == 'c' || tab[5] == 's' || tab[5] == 'p' || tab[5] == '%'))
-        tab[0] = 0;
-    if (tab[2] == 1 &&
-        (tab[5] == 'u' || tab[5] == 'o' || tab[5] == 'x' || tab[5] == 'X' ||
-            tab[5] == 'c' || tab[5] == 's' || tab[5] == 'p' || tab[5] == '%'))
-        tab[2] = 0;
-    if (tab[3] == 1 &&
-        (tab[5] == 'u' || tab[5] == 'd' || tab[5] == 'i' || tab[5] == 'c' ||
-            tab[5] == 's' || tab[5] == 'p' || tab[5] == '%'))
-        tab[3] = 0;
-    if (tab[4] == 1 &&
-        (tab[5] == 'c' || tab[5] == 's' || tab[5] == 'p' || tab[5] == '%'))
-        tab[4] = 0;
+    correct_flags(tab);
+    correct_flags2(tab);
+    if (tab[0] == 1 && nb >= 0)
+        function_pos(nb_char, '+');
+    if (tab[1] == 1)
+        function_alignement(nb, nb_char, tab[6]);
+    if (tab[2] == 1  && nb >= 0)
+        function_pos(nb_char, ' ');
+    if (tab[3] == 1)
+        function_sharp(nb_char, tab[5]);
+    if (tab[4] == 1)
+        function_spaces('0', tab[6], nb_char, ...);
+    if (tab[6] > 0)
+        function_space(' ', tab[6], nb_char, ...);
 }
 
-void redirect_flag(int *tab, int *nb_char)
+void redirect_flag_unb(int *tab, unsigned int nb, int *nb_char)
 {
-    int nb = 0;
     correct_flags(tab);
     correct_flags2(tab);
     if (tab[0] == 1)
-        function_pos(nb, nb_char, '+');
+        function_pos(nb_char, '+');
     if (tab[1] == 1)
         function_alignement(nb, nb_char, tab[6]);
     if (tab[2] == 1)
