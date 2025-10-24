e3965d9b03e03530cb2c80e7ead3e01d48bd861e
Eva Belanger <eva.belanger@epitech.eu>
2025-10-24 18:41:01 +0200

    :sparkles: create and finishe counting float characters for width

--- arguments/len_additionnal_flags_dbl.c
+++ arguments/len_additionnal_flags_dbl.c
@@ -5,9 +5,42 @@
 ** Check and return either 0 or the width less number of characters printed
 */
 
+static int count_floatlen(double ten, double nb, double temp, int nb_char)
+{
+    while (nb >= 1.0){
+        while (temp >= 10.0){
+            temp = temp / 10.0;
+            ten *= 10;
+        }
+        nb = nb - (int) temp * ten / 10;
+        nb_char++;
+        temp = nb;
+        ten = 10;
+    }
+    nb_char++;
+    for (int i = 0; i < 6; i++){
+        nb *= 10;
+        nb_char++;
+        nb -= (int) nb;
+    }
+    return nb_char;
+}
+
 int my_floatlen(double nb)
 {
-    return 0;
+    int nb_char = 0;
+    int i = 0;
+
+    if ((int) nb == 0)
+        nb_char = nb_char + 1;
+    if (nb < 0.0){
+        nb_char = nb_char + 1;
+        nb -= 0.0000005;
+        nb = - nb;
+    } else
+        nb += 0.0000005;
+    nb_char += count_floatlen(10, nb, nb, nb_char);
+    return nb_char;
 }
 
 int my_glen(double nb)
