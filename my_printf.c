8c4f9151760867dad93b4cb124fd35a10371489d
Stanislas Gaumain <stanislas.gaumain@epitech.eu>
2025-10-24 14:50:06 +0200

    :bug: correcting some flags to work in special cases

--- my_printf.c
+++ my_printf.c
@@ -39,33 +39,31 @@ void is_flag(int *tab, char *flags, char charact)
             tab[j] = 1;
 }
 
-int find_flags(const char *format, linked_list_t *l_list, va_list list, int *i)
+void find_flags(const char *format, int *nb_char, va_list list, int *i)
 {
-    int nb_char = 0;
+    linked_list_t l_list = *initialize_list();
     int tab[8] = {0};
     char flags[5] = {'+', '-', ' ', '#', '0'};
     int width = 0;
     int precision = 0;
     int j = 0;
 
-    while (format[*i] != l_list->letter && format[*i] != 0){
+    while (format[*i] != l_list.letter && format[*i] != 0){
         is_flag(tab, flags, format[*i]);
         find_number(i, format, &width, &precision);
-        if (l_list->letter == 'A')
+        if (l_list.letter == 'A')
             *i = *i + 1;
-        l_list = l_list->next;
+        l_list = *l_list.next;
     }
-    put_args_in_tab(tab, l_list->letter, width, precision);
+    put_args_in_tab(tab, l_list.letter, width, precision);
     if (format[*i] != 0)
-        l_list->ptr(list, &nb_char, tab);
-    return (nb_char);
+        l_list.ptr(list, nb_char, tab);
 }
 
 int my_printf(const char *format, ...)
 {
     va_list list;
     int nb_char = 0;
-    linked_list_t l_list = *initialize_list();
 
     va_start(list, format);
     if (format[0] == 0){
@@ -73,7 +71,7 @@ int my_printf(const char *format, ...)
     }
     for (int i = 0; format[i] != 0; i++){
         if (format[i] == '%'){
-            nb_char += find_flags(format, &l_list, list, &i);
+            find_flags(format, &nb_char, list, &i);
         } else {
             nb_char++;
             my_putchar(format[i]);
