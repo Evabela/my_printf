d075685df2e83fb7de0a0429c55ebaf0e18e1d13
Stanislas Gaumain <stanislas.gaumain@epitech.eu>
2025-10-24 11:42:29 +0200

    :sparkles: creating a file for the flag functions and correcting all the errors so that it works

---
+++ flags_functions.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2025
+** my_printf
+** File description:
+** initialize the flag functions
+*/
+#include "./include/my.h"
+
+void function_spaces(char letter, int *nb_char, int len)
+{
+}
+
+void function_sharp(int *nb_char, int tab)
+{
+}
+
+void function_pos(int *nb_char, char letter)
+{
+}
+
+void function_alignement(int nb, int *nb_char, int tab)
+{
+}
--- real_flags.c
+++ real_flags.c
@@ -8,26 +8,20 @@
 
 void redirect_flag_nb(int *tab, int nb, int *nb_char)
 {
-    int char_nb = 0;
-
     correct_flags(tab);
     correct_flags2(tab);
     if (tab[0] == 1 && nb >= 0)
         function_pos(nb_char, '+');
     if (tab[1] == 1)
-        function_alignement(nb, nb_char, tab[6]); // peut-être plus appeler une fonction qui récupère un nombre de charactère en fonction de ce qui est affiché et le passe en param
-    if (tab[2] == 1  && nb >= 0)
+        function_alignement(nb_char, tab[6]);
+    if (tab[2] == 1 && nb >= 0)
         function_pos(nb_char, ' ');
     if (tab[3] == 1)
         function_sharp(nb_char, tab[5]);
-    if (tab[4] == 1) {
-        char_nb = tab[6] - my_int_len(nb); 
-        function_spaces('0', nb_char, char_nb);
-    }
-    if (tab[6] > 0) {
-        char_nb = tab[6] - my_int_len(nb);
-        function_space(' ', nb_char, char_nb);
-    }
+    if (tab[4] == 1)
+        function_spaces('0', nb_char, tab[6] - my_intlen(nb));
+    if (tab[6] > 0)
+        function_spaces(' ', nb_char, tab[6] - my_intlen(nb));
 }
 
 void redirect_flag_dblnb(int *tab, double nb, int *nb_char)
@@ -37,15 +31,15 @@ void redirect_flag_dblnb(int *tab, double nb, int *nb_char)
     if (tab[0] == 1 && nb >= 0)
         function_pos(nb_char, '+');
     if (tab[1] == 1)
-        function_alignement(nb, nb_char, tab[6]);
-    if (tab[2] == 1  && nb >= 0)
+        function_alignement(nb_char, tab[6]);
+    if (tab[2] == 1 && nb >= 0)
         function_pos(nb_char, ' ');
     if (tab[3] == 1)
         function_sharp(nb_char, tab[5]);
     if (tab[4] == 1)
-        function_spaces('0', tab[6], nb_char, ...);
+        function_spaces('0', nb_char, tab[6] - my_intlen(nb));
     if (tab[6] > 0)
-        function_space(' ', tab[6], nb_char, ...);
+        function_spaces(' ', nb_char, tab[6] - my_intlen(nb));
 }
 
 void redirect_flag_unb(int *tab, unsigned int nb, int *nb_char)
@@ -55,13 +49,13 @@ void redirect_flag_unb(int *tab, unsigned int nb, int *nb_char)
     if (tab[0] == 1)
         function_pos(nb_char, '+');
     if (tab[1] == 1)
-        function_alignement(nb, nb_char, tab[6]);
+        function_alignement(nb_char, tab[6]);
     if (tab[2] == 1)
-        function_pos(nb, nb_char, ' ');
+        function_pos(nb_char, ' ');
     if (tab[3] == 1)
         function_sharp(nb_char, tab[5]);
     if (tab[4] == 1)
-        function_spaces('0', tab[6], nb_char, ...);
+        function_spaces('0', nb_char, tab[6] - my_intlen(nb));
     if (tab[6] > 0)
-        function_space(' ', tab[6], nb_char, ...);
+        function_spaces(' ', nb_char, tab[6] - my_intlen(nb));
 }
